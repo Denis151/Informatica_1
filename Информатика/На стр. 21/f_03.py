8a9f0e74d3c593b210a5c3f737da0d1c839fc148
Denis151 <56153920+Denis151@users.noreply.github.com>
2019-10-04 18:13:37 +0300

    Add files via upload

---
+++ Информатика/На стр. 21/f_03.py
@@ -0,0 +1,8 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a1 = [5,20]
+>>> a2 = [5,20]
+>>> a = 133
+>>> [a] + a1
+[133, 5, 20]
+>>> 
